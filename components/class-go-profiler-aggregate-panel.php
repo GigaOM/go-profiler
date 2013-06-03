37cbe29afc17b39f70d528167d71ba1bfa3724b2
Stephen Page <stephen.page@gigaom.com>
2013-06-03 12:17:52 -0300

    Update class-go-profiler-aggregate-panel.php
    fixed brace style

--- components/class-go-profiler-aggregate-panel.php
+++ components/class-go-profiler-aggregate-panel.php
@@ -4,13 +4,16 @@
 * adds a new panel for aggregate hook info
 */
 
-class go_profiler_aggregate_panel extends Debug_Bar_Panel {
+class go_profiler_aggregate_panel extends Debug_Bar_Panel
+{
 	
-	public function init() {
+	public function init() 
+	{
 		$this->title( __( 'Aggregated Hook Usage', 'debug-bar' ) );
 	}
 
-	public function render(){
+	public function render()
+	{
 		?>
 			<table id='debug_aggregate_table' style='font-size:12px'>
 				<tr>
