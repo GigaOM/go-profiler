b12ad5e5e46f8eb96fbd225ac2f1232c30c49c94
Stephen Page <stephen.page@gigaom.com>
2013-06-03 12:04:19 -0300

    Update go-profiler.js
    added TODO

--- components/js/go-profiler.js
+++ components/js/go-profiler.js
@@ -4,7 +4,7 @@ var GO_Profiler_Debug = function GO_Profiler_Debug() {
 		if( go_profiler_data ){
 
 			var profiler_data = jQuery.parseJSON( go_profiler_data );
-			
+			//@TODO: move the templates to appropriate php files
 			var go_profiler_summary_template = Mustache.compile("{{#summary}}<h2><span>TOTAL HOOKS</span>{{total_hooks}}</h2><h2><span>MOST MEMORY INTENSIVE</span>{{max_mem}} MB</h2><h2><span>LONGEST RUNNING</span>{{longest_hook}} seconds </h2><h2><span>USED MOST OFTEN</span>{{most_often}}</h2>{{/summary}}");
 			var summary_row = go_profiler_summary_template(profiler_data);
 			$( "#debug-menu-target-go_profiler_hook_panel" ).prepend( summary_row );
