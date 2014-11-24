644fcd4f8b397d2f219ce479bfeaed090592eb50
Casey Bisson <casey.bisson@gmail.com>
2014-11-24 12:48:55 -0800

    fixed plugin header
    reorganized plugin loader to eliminate nesting

--- go-profiler.php
+++ go-profiler.php
@@ -1,22 +1,27 @@
 <?php
 /*
-Name: Gigaom Execution Profiler
+Plugin Name: Gigaom Execution Profiler
 Plugin URI: http://kitchen.gigaom.com/
 Description: Profiles the exection flow within WordPress
 Version: 1.0
 Author: Casey Bisson
-Author URI: http://maisonbisson.com/blog/
+Author URI: http://maisonbisson.com/
 */
 
-// only loads if WP_DEBUG is defined and TRUE
-if ( defined( 'WP_DEBUG' ) && WP_DEBUG )
+// only load if WP_DEBUG is defined and TRUE
+if (
+	! defined( 'WP_DEBUG' ) ||
+	! WP_DEBUG
+)
 {
-	//need this set to gather query data
-	if ( ! defined( 'SAVEQUERIES' ) )
-	{
-		define( 'SAVEQUERIES', TRUE );
-	}//end if
-
-	require __DIR__ . '/components/class-go-profiler.php';
-	go_profiler();
+	return;
 }
+
+//need this set to gather query data
+if ( ! defined( 'SAVEQUERIES' ) )
+{
+	define( 'SAVEQUERIES', TRUE );
+}//end if
+
+require __DIR__ . '/components/class-go-profiler.php';
+go_profiler();
